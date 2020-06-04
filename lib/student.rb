1a9b01887445aaf4167d9844d0ca96f67e79c9b4
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 02:13:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,12 @@ class Student
       self.update
     else
       sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
       
+      DB[:conn].execute("SELECT last_insert_rowid() FORM students")[0][0]
+    end
   end
 
 
