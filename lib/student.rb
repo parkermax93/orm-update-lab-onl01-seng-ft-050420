b95a7a7361e58bd5beb796a330dfb7509a63b533
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 02:02:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,11 @@ class Student
     @grade = grade 
   end
   
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students ( )
+    
+  end
 
 
 end
